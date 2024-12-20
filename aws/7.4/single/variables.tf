d44d6c040a08ebfa69b41a42af460f33a63267a7
jbalan7576 <jbalan_7576@yahoo.com>
2024-12-20 18:12:29 +0530

    Comment Access/Secret Key

--- aws/7.4/single/variables.tf
+++ aws/7.4/single/variables.tf
@@ -1,9 +1,9 @@
 //AWS Configuration
-variable "access_key" {}
-variable "secret_key" {}
+#variable "access_key" {}
+#variable "secret_key" {}
 
 variable "region" {
-  default = "ap-south-1"
+  default = "ap-south-2"
 }
 
 // Availability zones for the region
